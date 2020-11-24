5fb8a76d2a5a48820d37de019943019a88ed28eb
B3thj <bery.sweet.23@gmail.com>
2020-11-24 13:33:30 -0600

    Avance en angular 10

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+  listaCurso: string[] = ['Typescript', 'Javascript', 'java SE','C#', 'php'];
+
+  habilitar: boolean =true;
+  constructor() { }
+
+setHabilitar(): void{
+  this.habilitar = (this.habilitar == true)? false: true;
+}
+}
