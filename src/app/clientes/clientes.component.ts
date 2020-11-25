563de15f624d4e177b8e13e8df0a03c4b7d7648b
B3thj <bery.sweet.23@gmail.com>
2020-11-24 23:25:42 -0600

    Visualizacion de datos en una tabla

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente' ;
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
 
+  clientes: Cliente[] = [
+    {id:1, nombre:'Beth', apellido:'Hdez', email:'test@tes.com', createAt:'2019-12-03'},
+    {id:2, nombre:'Adri', apellido:'Hdez', email:'test@tes.com', createAt:'2018-12-03'},
+    {id:3, nombre:'Roci', apellido:'Hdez', email:'test@tes.com', createAt:'2017-12-03'},
+    {id:4, nombre:'Ange', apellido:'Hdez', email:'test@tes.com', createAt:'2016-12-03'}
+  ];
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
