21b59d1c412e994d64eff7b8674f11a62c293c1d
B3thj <bery.sweet.23@gmail.com>
2020-11-25 21:46:45 -0600

    Agregando funcionalidad routing

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,8 +1,9 @@
+
 import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente' ;
-import {Observable} from 'rxjs/Observable';
-import { of } from 'rxjs';
+import {Observable, of} from 'rxjs';
+//import { of } from 'rxjs';
 
 @Injectable()
 export class ClienteService {
